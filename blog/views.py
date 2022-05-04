27e14a1faa83c95239a292f811fb556dfa408e38
Hyo <kyh12180@gmail.com>
2022-05-04 19:24:50 +0900

    FBV Blog Practice

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+# Function Based View
+
+def index(request) : 
+    # Get Query
+    posts = Post.objects.all()
+
+    return render (
+        request,
+        'blog/index.html',
+        {
+            'posts': posts,
+        }
+    )

08ca930a33ab186f0b27af95b802be3c836b9477
Hyo <kyh12180@gmail.com>
2022-05-04 19:34:07 +0900

    FBV Blog Detil Page

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index), 
+    # if integer -> call single_postpage
+    path('<int:pk>/', views.single_post_page),
 ]   

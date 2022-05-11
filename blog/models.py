6bfa2df27866253ed01e705671f8d5155ed35d4c
Hyo <kyh12180@gmail.com>
2022-05-12 01:37:02 +0900

    Author 필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from distutils.command.upload import upload
 from django.db import models
+from django.contrib.auth.models import User
 import os
 
 # Create your models here.
@@ -7,7 +8,6 @@ class Post(models.Model) :
     title = models.CharField(max_length = 30)
     hook_text = models.CharField(max_length = 100, blank=True)
     content = models.TextField()
-
 
     # Save Image
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True)
@@ -17,9 +17,12 @@ class Post(models.Model) :
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
 
+    # Author
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     # pk : 기본키
     def __str__(self) :
-        return f'[{self.pk}] {self.title}'
+        return f'[{self.pk}] {self.title}::{self.author}'
 
     def get_absolute_url(self) :
         return f'/blog/{self.pk}/'

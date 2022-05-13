e3d13488453a4a3f809770b2c99b2e665e634c8c
Hyo <kyh12180@gmail.com>
2022-05-14 00:21:17 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import redirect, render
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Category, Post, Tag
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
@@ -59,6 +60,19 @@ def tag_page(request, slug) :
             'no_category_post_count' : Post.objects.filter(category=None).count(), 
         }
     )
+
+class PostCreate(LoginRequiredMixin, CreateView) :
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
+
+    def form_valid(self, form) :
+        current_user = self.request.user
+        if current_user.is_authenticated :
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else :
+            return redirect('/blog/')
+
 
 
 '''

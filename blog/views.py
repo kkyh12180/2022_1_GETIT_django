aabe2a1804cc37a2aba7a8cf540355105bb6eae4
Hyo <kyh12180@gmail.com>
2022-05-13 22:17:59 +0900

    태그 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Category, Post
+from .models import Category, Post, Tag
 
 # Create your views here.
 
@@ -44,6 +44,22 @@ def category_page(request, slug) :
             
         }
     )
+
+def tag_page(request, slug) :
+    tag = Tag.objects.get(slug=slug)
+    post_list = Post.objects.filter(category=None)
+
+    return render (
+        request,
+        'blog/post_list.html',
+        {
+            'post_list' : post_list,
+            'tag' : tag, 
+            'categories' : Category.objects.all(), 
+            'no_category_post_count' : Post.objects.filter(category=None).count(), 
+        }
+    )
+
 
 '''
 # Function Based View
